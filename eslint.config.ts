b7c124d28406de08834033a71220a1950c155d4a
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-12-31 06:30:11 +0900

    :technologist: Setup eslint

---
+++ eslint.config.ts
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginHooks from "eslint-plugin-react-hooks";
+import pluginUnicorn from "eslint-plugin-unicorn";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{ts,tsx}"],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  pluginHooks.configs.flat.recommended,
+  pluginUnicorn.configs.recommended,
+  {
+    rules: {
+      "unicorn/filename-case": "off",
+      "unicorn/no-null": "off",
+      "unicorn/prevent-abbreviations": "off",
+    },
+  },
+]);
