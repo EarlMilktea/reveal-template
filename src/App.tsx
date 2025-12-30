88ab5759519c4a7bace6a11e68d1b8121ce10ca6
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-12-31 05:57:43 +0900

    :construction: Setup placeholders

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+/// <reference types="vite/client" />
+
+import assert from "assert-ts";
+import { useEffect, useRef } from "react";
+import Reveal from "reveal.js";
+
+import "reveal.js/dist/reset.css";
+import "reveal.js/dist/reveal.css";
+
+export default function App() {
+  const deckDivRef = useRef<HTMLDivElement | null>(null);
+  const deckRef = useRef<Reveal.Api | null>(null);
+
+  useEffect(() => {
+    if (deckRef.current !== null) {
+      return;
+    }
+    assert(deckDivRef.current !== null);
+
+    deckRef.current = new Reveal(deckDivRef.current, {
+      history: true,
+    });
+    deckRef.current.initialize().then(() => {});
+
+    return () => {
+      deckRef.current?.destroy();
+      deckRef.current = null;
+    };
+  }, []);
+
+  return (
+    <div className="reveal" ref={deckDivRef}>
+      <div className="slides">
+        <section>Slide 1</section>
+        <section>Slide 2</section>
+      </div>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import assert from "assert-ts";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+const elem = document.getElementById("root");
+assert(elem !== null);
+
+createRoot(elem).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
