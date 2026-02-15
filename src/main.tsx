60254e72d1a5e57eb59247836a2a208f0a9db278
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-02-16 01:04:32 +0900

    :lipstick: Use vanilla-extract

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "./global.css.ts";
+
 const elem = document.querySelector("#root");
 assert(elem);
 
