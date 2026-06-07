e005fec631b8c5fab8203598f17871d8cf09b56d
SS <66886825+EarlMilktea@users.noreply.github.com>
2026-06-07 13:51:36 +0900

    💩 Use react version manually
    Due to bug in eslint react plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -28,7 +28,7 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: "detect",
+        version: "19", // Temporary workaround: should be "detect"
       },
     },
   },
